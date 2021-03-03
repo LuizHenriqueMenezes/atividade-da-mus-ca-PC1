daf4a45d41f7d3c52863f4ce0c6f08a4fd220527
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-03 13:29:36 -0300

    Final da musíca

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace atividade_da_mus_ca_PC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.Blue;
+            Console.WriteLine("Vida Real - Paulo Ricardo");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Se você pudesse me dizer");
+            Console.WriteLine("Se você soubesse o que fazer");
+            Console.WriteLine("O que você faria");
+            Console.WriteLine("Aonde iria chegar?");
+            Console.WriteLine();
+            Console.WriteLine("Se você soubesse quem você é");
+            Console.WriteLine("Até onde vai a sua fé");
+            Console.WriteLine("O que você faria?");
+            Console.WriteLine("Pagaria pra ver");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("Se pudesse escolher");
+            Console.WriteLine("Entre o bem e o mal, ser ou não ser?");
+            Console.WriteLine("Se querer é poder");
+            Console.WriteLine("Tem que ir até o final");
+            Console.WriteLine("Se quiser vencer");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Se pudesse eu te levaria");
+            Console.WriteLine("Até onde você quer chegar");
+            Console.WriteLine("Brilho das estrelas");
+            Console.WriteLine("No primeiro lugar");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("Se pudesse escolher");
+            Console.WriteLine("Entre o bem e o mal, ser ou não ser?");
+            Console.WriteLine("Se querer é poder");
+            Console.WriteLine("Tem que ir até o final");
+            Console.WriteLine("Se quiser vencer");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("O mundo é perigoso");
+            Console.WriteLine("E cheio de armadilhas");
+            Console.WriteLine("Um dia estéril e gozo");
+            Console.WriteLine("Verdades e mentiras");
+            Console.WriteLine();
+            Console.WriteLine("Viver é quase um jogo");
+            Console.WriteLine("Um mergulho no infinito");
+            Console.WriteLine("Se souber brincar com fogo");
+            Console.WriteLine("Não há nada mais bonito");
+            Console.WriteLine("Se pudesse escolher");
+            Console.WriteLine("Entre o bem e o mal, ser ou não ser?");
+            Console.WriteLine("Se querer é poder");
+            Console.WriteLine("Tem que ir até o final");
+            Console.WriteLine("Se quiser vencer");
+            Console.WriteLine();
+
+
+
+
+
+        }
+    }
+}
